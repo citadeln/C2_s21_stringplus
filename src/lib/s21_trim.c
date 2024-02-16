ce2ec80a84e4df83789b01e53fe3278ba4c5a81c
Monroebu <monroebu@student.21-school.ru>
2024-02-16 15:13:58 +0500

    Убрала лишние принтефы

--- src/lib/s21_trim.c
+++ src/lib/s21_trim.c
@@ -16,9 +16,7 @@ int check_entrance(const char *src, const char *trim_chars, size_t i) {
 }
 
 int check_end(const char *s, const char *trim_chars, size_t j) {
-  printf("(count_i_right (j) = %ld\n", j);
   for (size_t q = 0; q < strlen(trim_chars); q++) {
-    printf("(count_i_right (j) = %ld\n", j);
     if (s[j] == trim_chars[q]) {
       j--;
       j = check_end(s, trim_chars, j);
@@ -28,7 +26,6 @@ int check_end(const char *s, const char *trim_chars, size_t j) {
 }
 
 void *trim(const char *src, const char *trim_chars) {
-  printf("src = %s \t trim_chars = %s\n", src, trim_chars);
   if (src == S21_NULL)
     return S21_NULL;
   else {
@@ -38,37 +35,17 @@ void *trim(const char *src, const char *trim_chars) {
       strncpy(s, src, strlen(src));
 
     else {
-      size_t i = check_entrance(
-          src, trim_chars,
-          0);  // считаем лишние символы и удаляем trim_chars с начала строки:
-      printf("strlen(trim_chars) = %ld\tstrlen(src) = %ld\t i = %ld\t s = %s\n",
-             strlen(trim_chars), strlen(src), i, s);
+      // считаем лишние символы и удаляем trim_chars с начала строки:
+      size_t i = check_entrance(src, trim_chars, 0);
 
       size_t j = 0;
-      for (int k = 0; j + i < strlen(src); j++, k++) {
-        // printf("s = %s\n", s);
-        s[k] = src[j + i];
-        // printf("s = %s\n", s);
-      }
+      for (int k = 0; j + i < strlen(src); j++, k++) s[k] = src[j + i];
 
-      printf("i = %ld\t j = %ld\ts = %s\n", i, j, s);
-
-      size_t count_i_right =
-          check_end(s, trim_chars, j - 1);  // указывает на последний символ
-                                            // (его порядковый номер в строке)
-
-      printf("i = %ld\t count_i_right = %ld\ts = %s\n", i, count_i_right, s);
-
+      // указывает на последний символ (его порядковый номер в строке)
+      size_t count_i_right = check_end(s, trim_chars, j - 1);
+      
       s[count_i_right + 1] = '\0';
-
-      printf("s = %s\n", s);
-
-      printf(
-          "\033[34m____next test____\n"
-          "\n\033["
-          "39m");
     }
-
     return s;
   }
 }
--- src/s21_string.h
+++ src/s21_string.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 #define S21_STRING_H
-#define S21_NULL ((void *)(0))
+#define S21_NULL ((void *)0)
 
 void *trim(const char *src, const char *trim_chars);
 
