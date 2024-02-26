059e93ade71c858ee1140e3f0ececb03ee7e5840
Monroebu <monroebu@student.21-school.ru>
2024-02-26 13:39:24 +0500

    накидала примерно sscanf

---
+++ src/lib/s21_sscanf.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include "../s21_string.h"
+
+int s21_sscanf(const char *str, const char *format, ...) {
+
+//определить тип по форматной строке
+/*
+%
+флаг +-пробел#0
+ширина цифра
+точность (перед ней точка)
+длинна (hlL)
+спецификатор (s )
+*/
+
+while (f) {
+    if (f == '%')
+    //создать структуру
+    else f++;
+}
+
+// обработка str
+/*
+пример №1
+"Hello w123"
+"%6s%c%d"
+*/
+char *s = (char)str;
+
+/*
+пример №2
+char *a;
+char b;
+int c;
+sscanf("Hello w 123", "%*s%c%*d", 5, a, &b, 2, &c)
+*/
+
+
+
+}
